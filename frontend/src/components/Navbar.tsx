f14784340da01a1f5ad4ce6721a630d0c142f3bd
nikhil kumar <nikhilkrsolanki@gmail.com>
2024-01-07 15:46:57 +0530

    created navigation bar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,31 +1,61 @@
 import logo from "../assets/images/logo.png";
-// mui icon
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
-import ArrowRightAltOutlinedIcon from '@mui/icons-material/ArrowRightAltOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { useState } from "react";
 
-export default function Navbar() {
-    return (
-        <header className="flex justify-between absolute left-0 right-0 top-6">
-            {/* logo */}
-            <img src={logo} alt="fmovies-logo" width={130}/>    
+export default function Navbar(){
+    const [showSearchBar, setShowSearchBar] = useState(false)
+    return(
+        <nav className="absolute top-4 left-4 right-4">
+            <div className="flex justify-between items-center">
+                {/* fmovies logo */}
+                <div className="w-36">
+                    <img src={logo} alt="fmovies-logo" className="object-cover"/>
+                </div>
 
-            {/* search bar */}
-            <form className="flex items-center space-x-1 bg-black opacity-80 p-2 rounded-full hover:opacity-100 hover:shadow-2xl">
-                <button className="bg-f-dark-zinc text-f-light-zinc flex items-center py-1.5 px-2 rounded-full text-sm">
-                    <FilterAltOutlinedIcon fontSize="small"/>
-                    filter
+                {/* search bar for larger screen devices */}
+                <div className="hidden sm:flex flex-1 max-w-xl px-3">
+                    <SearchBar/>
+                </div>
+
+                {/* show search bar button for mobile devices */}
+                <button className="flex sm:hidden text-f-blue" onClick={()=>{setShowSearchBar(!showSearchBar)}}>
+                    <SearchOutlinedIcon/>
                 </button>
-                <input className="focus:outline-none bg-inherit text-f-blue text-center text-md w-96" placeholder="Search movies..."/>
-                <button className="text-f-blue pr-2">
+
+                {/* login button */}
+                <button className="flex items-center space-x-1 text-white sm:border-2 p-1 px-4 rounded-full sm:hover:bg-f-blue hover:border-f-light-blue hover:text-black duration-150 transition-colors border-white">
+                    <span className="hidden sm:flex">Login</span>
+                    <ArrowForwardOutlinedIcon/>
+                </button>
+            </div>
+
+            {/* search bar for small screen devices */}
+            <div className="flex sm:hidden pt-2">
+                {showSearchBar && <SearchBar/>}
+            </div>
+        </nav>
+    )
+}
+
+
+// search bar
+function SearchBar(){
+    return(
+        <div className="bg-black p-2 rounded-full opacity-70 hover:transition-opacity hover:opacity-100 duration-150 hover:shadow-lg flex-1">
+            <form className="flex items-center space-x-2">
+                <button className="bg-f-dark-zinc text-f-light-zinc pl-1 pr-2 py-0.5 rounded-full text-sm flex items-center">
+                    <FilterAltOutlinedIcon className="scale-75"/>
+                    Filter
+                </button>
+
+                <input placeholder="Search movies..." className="focus:outline-none w-full text-f-blue text-center bg-inherit"/>
+
+                <button className="text-f-blue">
                     <SearchOutlinedIcon/>
                 </button>
             </form>
-
-            {/* login button*/}
-            <button className="text-white bg-red-200">
-                Login <ArrowRightAltOutlinedIcon/>
-            </button>
-        </header>
-    );
+        </div>
+    )
 }
